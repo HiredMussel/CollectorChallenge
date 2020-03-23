e75346ca1004745b47444c90916a52b25e6c5604
HiredMussel <alexmusselwhite2@gmail.com>
2020-03-23 14:28:35 +0000

    write failure and malformed tests

--- tests/CardTest.php
+++ tests/CardTest.php
@@ -5,7 +5,7 @@ require_once '../Card.php';
 use PHPUnit\Framework\TestCase;
 
 class FunctionTest extends TestCase {
-    public function testSuccessCardContruct() {
+    public function testSuccessCardConstruct() {
         $cardTest = new Card ('Test Card', ['Test Key' => 'Test Value']);
         
         $expectedTitle = 'Test Card';
@@ -13,5 +13,23 @@ class FunctionTest extends TestCase {
 
         $this->assertEquals($cardTest->getTitle(), $expectedTitle);
         $this->assertEquals($cardTest->getAttrib('Test Key'), $expectedValue);
+    }
+
+    public function testFailureCardConstruct() {
+        $this->expectException(ArgumentCountError::class);
+
+        $cardTest = new Card ();
+    }
+
+    public function testMalformedTitleCardConstruct() {
+        $this->expectException(TypeError::class);
+
+        $cardTest = new Card ([0 => 0]);
+    }
+
+    public function testMalformedAttribsCardConstruct() {
+        $this->expectException(TypeError::class);
+
+        $cardTest = new Card('Test Title', 'Test Attribs');
     }
 }
