5fff4339e6120fb62e622a2e55926801e37dbb05
HiredMussel <alexmusselwhite2@gmail.com>
2020-03-23 16:35:33 +0000

    read entries from database

--- index.php
+++ index.php
@@ -19,10 +19,30 @@
     <div class="container">
         <?php
 
-            // This is currently a debug page to test the functionality of the "print card" function. Styling will be applied via
-            // a stylesheet later
+            // This is currently a debug page to test the functionality of the "print card" function.
+            // Basic styling and layout has now been added, but frontend may still be visually improved later.
 
             require_once 'Card.php';
+
+            // Load test card from database
+            $db = new PDO('mysql:host=db;dbname=collector_challenge', 'root', 'password');
+            $db -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+            $query = $db->prepare('SELECT `title`,`genre`,`completion`,`description` FROM `games`;');
+            $query->execute();
+
+            $result = $query->fetchAll();
+
+            // parse the return from the database into a card
+            foreach ($result as $entry) {
+                $cardTitle = $entry['title'];
+                unset($entry['title']);
+                if (isset($entry['completion'])) {
+                    $entry['completion'] .= '%';
+                }
+                $card = new Card($cardTitle, $entry);
+                $card->printCard();
+            }
 
             // Change evaluated condition in for loop to generate desired number of test cards
             $cardTest = [];
---
+++ toCard.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'card.php';
+
+/**
+ * Function to transform the result of a db query into a valid card
+ */
+function dbToCard(Array $queryResponse) : Card {
+
+
+    return $
+}
