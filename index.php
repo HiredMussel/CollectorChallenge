b31475e61cf1d578e982718437c44709153c4772
HiredMussel <alexmusselwhite2@gmail.com>
2020-03-23 14:32:17 +0000

    tidy up commenting

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 require_once 'Card.php';
 
-$cardTest = new Card('Test Card');
+$cardTest = new Card('Test Card', ['Test Key' => 'Test Value']);
 
 $cardTest->printCard();
--- tests/CardTest.php
+++ tests/CardTest.php
@@ -5,6 +5,9 @@ require_once '../Card.php';
 use PHPUnit\Framework\TestCase;
 
 class FunctionTest extends TestCase {
+    /**
+     * Test that the card type can be constructed successfully. Test both the setting of the title and the card attributes
+     */
     public function testSuccessCardConstruct() {
         $cardTest = new Card ('Test Card', ['Test Key' => 'Test Value']);
         
@@ -15,12 +18,18 @@ class FunctionTest extends TestCase {
         $this->assertEquals($cardTest->getAttrib('Test Key'), $expectedValue);
     }
 
+    /**
+     * The only failure cases for the constructor are either an empty input or a malformed input. Failure test is for empty input
+     */
     public function testFailureCardConstruct() {
         $this->expectException(ArgumentCountError::class);
 
         $cardTest = new Card ();
     }
 
+    /**
+     * Separate tests for the cases of a malformed title and attribute array respectively
+     */
     public function testMalformedTitleCardConstruct() {
         $this->expectException(TypeError::class);
 
