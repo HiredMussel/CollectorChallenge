cff2618c78e8f0ef6c743c9c3167ab40b84529dc
HiredMussel <alexmusselwhite2@gmail.com>
2020-03-24 10:30:14 +0000

    finish form

--- curate.php
+++ curate.php
@@ -21,15 +21,19 @@
 
         <h2>Add new game:</h2>
 
-        <form id="newGame">
+        <form method="post" id="newGame">
             <label for="title">Game Title:</title>
-            <input type="text" id="title" name="title">
+            <input type="text" id="title" name="title" required>
             <label for="genre">Genre:</title>
             <input type="text" id="genre" name="genre">
             <label for="completion">Completion:</title>
-            <input type="number" min="0" max="255" id="completion" name="completion">%<br>
+            <input type="number" min="0" max="255" id="completion" name="completion" value="0" required>%<br>
             <label for="description">Description:</title>
             <textarea id="description" name="description"></textarea>
+            <input type="submit" id="submit" name="submit">
         </form>
     </div>
+    <?php
+        var_dump($_POST);
+    ?>
 </body>
