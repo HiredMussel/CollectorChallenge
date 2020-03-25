763ab90d83d4852810c220583a7fe1a1cc840fb1
HiredMussel <alexmusselwhite2@gmail.com>
2020-03-25 11:51:16 +0000

    tidy code and comments

--- deleteItem.php
+++ deleteItem.php
@@ -2,8 +2,9 @@
 
 /**
  * This is a page whose sole purpose is to remove an item from the database and then redirect the user to the curate.php page.
+ * The flag 'is_deleted' prevents the game from appearing in the list of games on either index.php or curate.php. Since nothing
+ * confidential is being stored, this method is preferred as the restoration of unintentionally-deleted data is still possible.
  */
-
 require_once 'connectToDb.php';
 
 if (isset($_GET['gameId'])) {
--- listEditable.php
+++ listEditable.php
@@ -19,19 +19,25 @@ function listEditable() : String {
     $result = $query->fetchAll();
 
     $return = '';
-
+    /**
+     * If a title is set for a game, the relevant section of the page should have en entry added to it with a form
+     * allowing that game's information to be edited or the game to be removed from the database (since nothing confidential
+     * is being stored, this database uses a soft deletion)
+     */
     foreach ($result as $game) {
-        $return .=     '<div class=editGame>'
-                         . '<form method="get" action="./editEntry.php">'
-                             . '<input class="hidden" type="number" value="' . $game['id'] . '" id="gameId" name="gameId">'
-                             . '<h3>'. $game['title'] . '</h3>'
-                             . '<input class="editButton" type="submit" value="Edit">'
-                         . '</form>'
-                         . '<form method="get" action="./deleteItem.php">'
-                            . '<input class="hidden" type="number" value="' . $game['id'] . '" id="gameId" name="gameId">'
-                            . '<input class="deleteButton" type="submit" value="Delete">'
-                         . '</form>'
-                     . '</div>';
+        if (isset($game['title'])) {
+            $return .=     '<div class=editGame>'
+                            . '<form method="get" action="./editEntry.php">'
+                                . '<input class="hidden" type="number" value="' . $game['id'] . '" id="gameId" name="gameId">'
+                                . '<h3>'. $game['title'] . '</h3>'
+                                . '<input class="editButton" type="submit" value="Edit">'
+                            . '</form>'
+                            . '<form method="get" action="./deleteItem.php">'
+                                . '<input class="hidden" type="number" value="' . $game['id'] . '" id="gameId" name="gameId">'
+                                . '<input class="deleteButton" type="submit" value="Delete">'
+                            . '</form>'
+                        . '</div>';
+        }
     }
 
     return $return;
